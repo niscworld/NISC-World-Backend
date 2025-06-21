185e430cf05872df55072ec6599fc7544406aec7
NISC World <nisc.co.in@gmail.com>
2025-06-21 17:19:10 +0530

    Fix

--- app/routes.py
+++ app/routes.py
@@ -2,32 +2,8 @@ from flask import Blueprint
 
 app = Blueprint("app", __name__)
 
-def createUser():
-    # Create User
-    from .models import User, Profile, db
-    from .utils import get_current_time
-    user = User(user_id='SrinivasCharanK')
-    user.set_password('Srinivas')  # Replace with actual password
-    user.login_time = get_current_time()
-
-    # Create Profile
-    profile = Profile(
-        user_id='SrinivasCharanK',
-        fullname='Srinivas Charan K',
-        email='charan@example.com',
-        position='Developer'
-    )
-
-    # Add to DB
-    db.session.add(user)
-    db.session.add(profile)
-    db.session.commit()
-
-    print("✅ User created successfully!")
-
 @app.route("/")
 def home():
-    createUser()
     return """
     <h1>Welcome to NAKSH INNOVATIVE SOLUTIONS CONSULTANCY</h1>
     <p>Visit our website: <a href='https://www.nisc.co.in' target='_blank'>nisc.co.in</a></p>
